f153e16899edc2f539b8dc9805ca149b79b8e50a
inkxk <64063112+inkxk@users.noreply.github.com>
2021-11-24 14:31:35 +0700

    Add files via upload

---
+++ JavaBasic/Lab/Lab5.java
@@ -0,0 +1,33 @@
+public class Lab5 {
+
+    public static void main(String[] args) {
+
+        int score = 60;
+        String grade = "";
+
+        switch (score) {
+            case 80:
+                grade = "A";
+                break;
+            case 70:
+                grade = "B";
+                break;
+            case 60:
+                grade = "C";
+                break;
+            case 50:
+                grade = "D";
+                break;
+            case 40:
+                grade = "F";
+                break;
+            default:
+                grade = "E";
+                break;
+        }
+
+        System.out.println(grade);
+
+    }
+   
+}
