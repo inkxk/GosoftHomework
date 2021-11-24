75cdc6d474db0211a42344a3d43bd3ef858e53ee
inkxk <64063112+inkxk@users.noreply.github.com>
2021-11-24 15:09:49 +0700

    Update Lab8.java

--- JavaBasic/Lab/Lab8.java
+++ JavaBasic/Lab/Lab8.java
@@ -3,9 +3,9 @@ public class Lab8 {
         
         for (int count = 0; count <= 20; count++) {
             if (count == 11) {
-                System.out.println(count);
                 break;
             }
+            System.out.println(count);
         }
     }
 }
