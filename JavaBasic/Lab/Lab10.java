b90afc112d41ae0427922d861107ed89c8f9a1f9
inkxk <64063112+inkxk@users.noreply.github.com>
2021-11-24 15:14:45 +0700

    Add files via upload

---
+++ JavaBasic/Lab/Lab10.java
@@ -0,0 +1,10 @@
+public class Lab10 {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 10; count++) {
+            if (count % 2 == 0) {
+                continue;
+            }
+            System.out.println(count);
+        }
+    }
+}
