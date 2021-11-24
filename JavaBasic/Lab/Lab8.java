35fe19db657329727c72b6c276476d4c6a423925
inkxk <64063112+inkxk@users.noreply.github.com>
2021-11-24 15:03:14 +0700

    Add files via upload

---
+++ JavaBasic/Lab/Lab8.java
@@ -0,0 +1,11 @@
+public class Lab8 {
+    public static void main(String[] args) {
+        
+        for (int count = 0; count <= 20; count++) {
+            System.out.println(count);
+            if (count == 11) {
+                break;
+            }
+        }
+    }
+}
