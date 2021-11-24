214c425bf48408cf0eaaf474a81d44bf19c223e7
inkxk <64063112+inkxk@users.noreply.github.com>
2021-11-24 15:12:30 +0700

    Add files via upload

---
+++ JavaBasic/Lab/Lab9.java
@@ -0,0 +1,13 @@
+public class Lab9 {
+    public static void main(String[] args) {
+
+        for (int count = 0; count <= 20; count++) {
+            if (count == 11) {
+                continue;
+            }
+            System.out.println(count);
+        }
+
+    }
+    
+}
