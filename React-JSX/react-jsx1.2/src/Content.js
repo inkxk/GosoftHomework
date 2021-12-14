3653bbf5a3bd97f06752bcd9abfb3ddebd25960d
Potcharapol Nokyoo <64063112+inkxk@users.noreply.github.com>
2021-12-14 17:18:40 +0700

    Add files via upload

---
+++ React-JSX/react-jsx1.2/src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Content() {
+    return (
+        <div>
+            <h1>This is Content</h1>
+            <p>created by using function</p>
+        </div>
+    )
+}
+
+export default Content
---
+++ React-JSX/react-jsx1.2/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <h1>This is Header</h1>
+            <p>created by using function</p>
+        </div>
+    )
+}
+
+export default Header
